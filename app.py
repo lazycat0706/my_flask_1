14acd565db23e7ad77a2751c9cbdc1909269eb01
lazycat0706 <236082202@qq.com>
2020-11-27 18:36:53 +0800

    9*9

--- app.py
+++ app.py
@@ -1,33 +1,15 @@
 from flask import Flask
 from flask import request
-from flask import redirect, url_for
+from flask import redirect, url_for, render_template
 from werkzeug.routing import BaseConverter
 
 app = Flask(__name__)
 
 
-class TelephoneConverter(BaseConverter):
-    regex = r'1[3-9][0-9]{9}'
-
-
-app.url_map.converters['tel'] = TelephoneConverter
-
-
-@app.route('/tel/<tel:tel_num>')
-def phonenum(tel_num):
-    return "welcom, {}".format(tel_num)
-
-
 @app.route('/')
 def index():
-    # return 'Hello World!'
-    # URL = url_for('login')
-    return redirect('/login/')
-
-
-@app.route('/user/<name>')
-def user(name):
-    return "hello12, {}".format(name)
+    exam = {'A': -5}
+    return render_template('index.html', info=exam)
 
 
 @app.route('/debug/')
@@ -35,27 +17,5 @@ def debug_info():
     return "Debug Test!"
 
 
-@app.route('/login/')
-def login():
-    info = """
-    <html>
-        <head>
-
-        </head>
-        <body>
-            <div>
-            <input type="button", value="name">
-            <input type="text", value="">
-            <input type="button", value="passwd">
-            <input type="text", value="">
-            <input type="button", value="submit">
-            </div>
-        </body>
-    </html>
-    """
-    return info
-
-
 if __name__ == '__main__':
-    # app.debug = True
     app.run(debug=True)
